f31e9ef0699b25e170af0ecca97bfc0c4fa7cc2b
WALYSON MENEZES DE SOUSA <55339015823@info.local>
2026-02-26 08:19:47 -0300

    Subindo atividade API

--- API/Filmes.WebAPI/obj/Debug/net10.0/Filmes.WebAPI.AssemblyInfo.cs
+++ API/Filmes.WebAPI/obj/Debug/net10.0/Filmes.WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Filmes.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7de5db0ad5d777f74cf6b3c57c2b142d62eed1f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Filmes.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Filmes.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
