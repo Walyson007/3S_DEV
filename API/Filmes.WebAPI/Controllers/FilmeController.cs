a5c101ddf45f8da4445180f58bb17695a4945458
WALYSON MENEZES DE SOUSA <55339015823@info.local>
2026-03-05 08:51:31 -0300

    Subindo API completa com banco de dados

--- API/Filmes.WebAPI/Controllers/FilmeController.cs
+++ API/Filmes.WebAPI/Controllers/FilmeController.cs
@@ -1,7 +1,10 @@
-﻿using Filmes.WebAPI.Interfaces;
+﻿using Filmes.WebAPI.DTO;
+using Filmes.WebAPI.Interfaces;
 using Filmes.WebAPI.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace Filmes.WebAPI.Controllers;
 
@@ -15,7 +18,20 @@ public class FilmeController : ControllerBase
     {
         _filmeRepository = filmeRepository;
     }
+    [HttpGet("{id}")]
+    public IActionResult GetById(Guid id)
+    {
+        try
+        {
+            return Ok(_filmeRepository.BuscarPorId(id));//Busca o filme pelo id e retorna o resultado
+        }
+        catch (Exception erro)
+        {
+            return BadRequest(erro.Message);//Retorna um status code 400 (Bad Request) com a mensagem de erro
+        }
+    }
 
+    //[Authorize]
     [HttpGet]
     public IActionResult Get()
     {
@@ -23,15 +39,45 @@ public class FilmeController : ControllerBase
         {
             return Ok(_filmeRepository.Listar());
         }
-        catch(Exception e)
+        catch (Exception e)
         {
             return BadRequest(e.Message);
         }
     }
 
     [HttpPost]
-    public IActionResult Post(Filme novoFilme)
+    public async Task<IActionResult> Post([FromForm] FilmeDTO filme)
     {
+        if (String.IsNullOrWhiteSpace(filme.Nome))
+            return BadRequest("O nome e o Gênero do filme é obrigatório.");
+
+        Filme novoFilme = new Filme();
+
+        if(filme.Imagem != null && filme.Imagem.Length != 0)
+        {
+          var extensao = Path.GetExtension(filme.Imagem.FileName);
+          var nomeArquivo = $"{Guid.NewGuid()}{extensao}";
+
+          var pastaRelativa = "wwwroot/Imagens";
+          var caminhoPasta = Path.Combine(Directory.GetCurrentDirectory(),pastaRelativa);
+
+          // Verifica se a pasta existe, caso contrário, cria a pasta
+          if(!Directory.Exists(caminhoPasta))
+              Directory.CreateDirectory(caminhoPasta);
+
+          var caminhoCompleto = Path.Combine(caminhoPasta, nomeArquivo);
+
+          using (var stream = new FileStream(caminhoCompleto, FileMode.Create))
+            {
+               await filme.Imagem.CopyToAsync(stream);
+            }
+
+          novoFilme.Imagem = nomeArquivo;
+        }
+
+        novoFilme.IdGenero = filme.IdGenero.ToString();
+        novoFilme.Titulo = filme.Nome;
+
         try
         {
             _filmeRepository.Cadastrar(novoFilme);
@@ -43,4 +89,102 @@ public class FilmeController : ControllerBase
         }
     }
 
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(Guid id, FilmeDTO filmeAtualizado)
+    {
+        var filmeBuscado = _filmeRepository.BuscarPorId(id);
+
+        if (filmeBuscado == null)
+            return NotFound("Filme não encontrado.");
+
+        if(!String.IsNullOrWhiteSpace(filmeAtualizado.Nome))
+            filmeBuscado.Titulo = filmeAtualizado.Nome;
+
+        if(filmeAtualizado.IdGenero != null && filmeBuscado.IdGenero != filmeAtualizado.IdGenero.ToString())
+           filmeBuscado.IdGenero = filmeAtualizado.IdGenero.ToString();
+
+        if(filmeAtualizado.Imagem != null && filmeAtualizado.Imagem.Length != 0)
+        {
+          var pastaRelativa = "wwwroot/Imagens";
+          var caminhoPasta = Path.Combine(Directory.GetCurrentDirectory(),pastaRelativa);
+
+          //Delta arquivo antigo
+          if(!String.IsNullOrEmpty(filmeBuscado.Imagem))
+          {
+            var caminhoAntigo = Path.Combine(caminhoPasta, filmeBuscado.Imagem);
+            if(System.IO.File.Exists(caminhoAntigo))
+                System.IO.File.Delete(caminhoAntigo);
+            }
+
+            //salvar nova imagem
+            var extensao = Path.GetExtension(filmeAtualizado.Imagem.FileName);
+            var nomeArquivo = $"{Guid.NewGuid()}{extensao}";
+
+            if(!Directory.Exists(caminhoPasta))
+              Directory.CreateDirectory(caminhoPasta);
+
+            var caminhoCompleto = Path.Combine(caminhoPasta, nomeArquivo);
+
+            using (var stream = new FileStream(caminhoCompleto, FileMode.Create))
+            {
+              await filmeAtualizado.Imagem.CopyToAsync(stream);
+            }
+            filmeBuscado.Imagem = nomeArquivo;
+        }
+
+        try
+        {
+            _filmeRepository.AtualizarIdUrl(id, filmeBuscado);
+
+            return NoContent();
+        }
+        catch(Exception erro)
+        {
+            return BadRequest(erro.Message);
+        }
+    }
+
+    [HttpPut]
+    public IActionResult Put(Filme filmeAtualizado)
+    {
+        try
+        {
+            _filmeRepository.AtualizarIdCorpo(filmeAtualizado);
+            return NoContent();
+        }
+        catch(Exception erro)
+        {
+            return BadRequest(erro.Message);
+        }
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(Guid id) 
+    {
+        var filmeBuscado = _filmeRepository.BuscarPorId(id);
+        if (filmeBuscado == null)
+            return NotFound("Filme não encontrado.");
+
+        var pastaRelativa = "wwwroot/Imagens";
+        var caminhoPasta = Path.Combine(Directory.GetCurrentDirectory(),pastaRelativa);
+
+        //Delta arquivo antigo
+        if(!String.IsNullOrEmpty(filmeBuscado.Imagem))
+        {
+            var caminho = Path.Combine(caminhoPasta, filmeBuscado.Imagem);
+
+            if(System.IO.File.Exists(caminho))
+                System.IO.File.Delete(caminho);
+        }
+
+        try
+        {
+            _filmeRepository.Deletar(id);
+            return NoContent();
+        }
+        catch(Exception erro)
+        {
+            return BadRequest(erro.Message);
+        }
+    }
 }
