16219d1b676c3941a059e44535f684b2f2d84412
WALYSON MENEZES DE SOUSA <55339015823@info.local>
2026-03-05 16:42:16 -0300

    Subindo projeto API junto ao Banco de Dados

---
+++ Event_Plus/EventPlus.WebAPI/Models/ComentarioEvento.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventPlus.WebAPI.Models;
+
+[Table("ComentarioEvento")]
+public partial class ComentarioEvento
+{
+    [Key]
+    [Column("Id_ComentarioEvento")]
+    public Guid IdComentarioEvento { get; set; }
+
+    [StringLength(200)]
+    [Unicode(false)]
+    public string Descricao { get; set; } = null!;
+
+    public bool Exibe { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime DataComentarioEvento { get; set; }
+
+    [Column("Id_Usuario")]
+    public Guid? IdUsuario { get; set; }
+
+    [Column("Id_Evento")]
+    public Guid? IdEvento { get; set; }
+
+    [ForeignKey("IdEvento")]
+    [InverseProperty("ComentarioEventos")]
+    public virtual Evento? IdEventoNavigation { get; set; }
+
+    [ForeignKey("IdUsuario")]
+    [InverseProperty("ComentarioEventos")]
+    public virtual Usuario? IdUsuarioNavigation { get; set; }
+}
---
+++ Event_Plus/EventPlus.WebAPI/Models/Instituicao.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventPlus.WebAPI.Models;
+
+[Table("Instituicao")]
+[Index("Cnpj", Name = "UQ__Institui__AA57D6B4B336A23B", IsUnique = true)]
+public partial class Instituicao
+{
+    [Key]
+    [Column("Id_Instituicao")]
+    public Guid IdInstituicao { get; set; }
+
+    [Column("Nome_Fantasia")]
+    [StringLength(100)]
+    [Unicode(false)]
+    public string NomeFantasia { get; set; } = null!;
+
+    [Column("CNPJ")]
+    [StringLength(14)]
+    [Unicode(false)]
+    public string Cnpj { get; set; } = null!;
+
+    [StringLength(100)]
+    [Unicode(false)]
+    public string Endereco { get; set; } = null!;
+
+    [InverseProperty("IdInstituicaoNavigation")]
+    public virtual ICollection<Evento> Eventos { get; set; } = new List<Evento>();
+}
